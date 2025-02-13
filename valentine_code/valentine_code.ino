e1c1675269de66ee60bdafee8ac01c85a9754b7c
guwoh <143280760+guwoh@users.noreply.github.com>
2025-02-13 23:01:04 +0700

    nothing

--- valentine_code/valentine_code.ino
+++ valentine_code/valentine_code.ino
@@ -1,7 +1,8 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-// let eat chocolat alone
+
+
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 #define OLED_RESET    -1 // Reset pin # (or -1 if sharing Arduino reset pin)
